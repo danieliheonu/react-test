60b298b86dc1e7ce0bebf6e2bc9ca64a0cd9f738
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2022-06-29 07:26:07 +0100

    fixed the card css issue

--- frontend/src/components/BookComponent.js
+++ frontend/src/components/BookComponent.js
@@ -3,12 +3,12 @@ import { books } from '../db/db'
 
 function BookComponent() {
   return (
-    <div><div className='popular-product-section book-section'>
-    <span className='title'>Top Sellers in Books for you</span>
-    <div>
-        {books.map(book => <img src={book.url} />)}
+    <div className='popular-product-section'>
+      <span className='title'>Top Sellers in Books for you</span>
+      <div>
+          {books.map(book => <img src={book.url} />)}
+      </div>
     </div>
-</div></div>
   )
 }
 
